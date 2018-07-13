9a205b1456c21ae1ad61e27207850e5b0d2c0f1a
Xuhang Chen <xuhangc@seas.upenn.edu>
2018-07-13 11:25:16 -0400

    retain name and python script

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -47,22 +47,30 @@ exports.login = function(req, res) {
 }
 
 exports.upload = function(req, res) {
-    // var files = req.files;
-    // for (var i = 0; i < files.length; i++) {
-    //   console.log('type %s', files[i].mimetype);
-    //   console.log('original name：%s', files[i].originalname);
-    //   console.log('size：%s', files[i].size);
-    //   console.log('save path：%s', files[i].path);
-    // }
-    // res.send({ret_code: '0'});
     var dir_name = Date.now();
     mkdirp('uploads/' + dir_name, function(err) {
-      var upload = multer({ dest: 'uploads/' + dir_name }).any();
+      var storage = multer.diskStorage({
+        destination: 'uploads/' + dir_name,
+        filename: function ( req, file, cb ) {
+            cb(null, file.originalname);
+        }
+      });
+      var upload = multer({ storage: storage }).any();
       upload(req, res, function(err) {
           if(err) {
               return res.end("Error uploading file.");
           }
-          res.end("File is uploaded");
+          else {
+            // var files = req.files;
+            // for (var i = 0; i < files.length; i++) {
+            //   console.log('type %s', files[i].mimetype);
+            //   console.log('original name：%s', files[i].originalname);
+            //   console.log('size：%s', files[i].size);
+            //   console.log('save path：%s', files[i].path);
+            // }
+            // res.send({ret_code: '0'});
+            return res.end("Done");
+          }
       });
     });
 }
