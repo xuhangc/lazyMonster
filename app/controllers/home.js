8b6863ff1c4890661deda9742563a7ba3ba8eb81
Xuhang Chen <xuhangc@seas.upenn.edu>
2018-07-13 11:57:01 -0400

    test py

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -47,30 +47,33 @@ exports.login = function(req, res) {
 }
 
 exports.upload = function(req, res) {
-    var dir_name = Date.now();
-    mkdirp('uploads/' + dir_name, function(err) {
-      var storage = multer.diskStorage({
-        destination: 'uploads/' + dir_name,
-        filename: function ( req, file, cb ) {
-            cb(null, file.originalname);
-        }
-      });
-      var upload = multer({ storage: storage }).any();
-      upload(req, res, function(err) {
-          if(err) {
-              return res.end("Error uploading file.");
-          }
-          else {
-            // var files = req.files;
-            // for (var i = 0; i < files.length; i++) {
-            //   console.log('type %s', files[i].mimetype);
-            //   console.log('original name：%s', files[i].originalname);
-            //   console.log('size：%s', files[i].size);
-            //   console.log('save path：%s', files[i].path);
-            // }
-            // res.send({ret_code: '0'});
-            return res.end("Done");
-          }
-      });
+    // var dir_name = Date.now();
+    // mkdirp('uploads/' + dir_name, function(err) {
+    //   var storage = multer.diskStorage({
+    //     destination: 'uploads/' + dir_name,
+    //     filename: function ( req, file, cb ) {
+    //         cb(null, file.originalname);
+    //     }
+    //   });
+    //   var upload = multer({ storage: storage }).any();
+    //   upload(req, res, function(err) {
+    //       if(err) {
+    //           return res.end("Error uploading file.");
+    //       }
+    //       else {
+    //         var files = req.files;
+    //         for (var i = 0; i < files.length; i++) {
+    //           console.log('type %s', files[i].mimetype);
+    //           console.log('original name：%s', files[i].originalname);
+    //           console.log('size：%s', files[i].size);
+    //           console.log('save path：%s', files[i].path);
+    //         }
+    //         res.send("Success.");
+    //       }
+    //   });
+    // });
+    PythonShell.run('py.py', function (err) {
+      if (err) throw err;
+      console.log('finished');
     });
 }
