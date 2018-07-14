a498724971a634cf5bc314cd448ba30db22292cf
Xuhang Chen <xuhangc@seas.upenn.edu>
2018-07-14 11:22:03 -0400

    integrate

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -48,44 +48,43 @@ exports.login = function(req, res) {
 }
 
 exports.upload = function(req, res) {
-    // var dir_name = Date.now();
-    // mkdirp('uploads/' + dir_name, function(err) {
-    //   var storage = multer.diskStorage({
-    //     destination: 'uploads/' + dir_name,
-    //     filename: function ( req, file, cb ) {
-    //         cb(null, file.originalname);
-    //     }
-    //   });
-    //   var upload = multer({ storage: storage }).any();
-    //   upload(req, res, function(err) {
-    //       if(err) {
-    //           return res.end("Error uploading file.");
-    //       }
-    //       else {
-    //         var files = req.files;
-    //         for (var i = 0; i < files.length; i++) {
-    //           console.log('type %s', files[i].mimetype);
-    //           console.log('original name：%s', files[i].originalname);
-    //           console.log('size：%s', files[i].size);
-    //           console.log('save path：%s', files[i].path);
-    //         }
-    //         res.send("Success.");
-    //       }
-    //   });
-    // });
-    var options = {
-      mode: 'json',
-      pythonOptions: ['-u'], // get print results in real-time
-      scriptPath: path.join(__dirname + "../../../python"),
-      args: ['value1', 'value2']
-    };
-    var shell = new PythonShell('py.py', options);
-    shell.on('message', function (message) {
-      console.log(message);
-    });
-    shell.end(function (err) {
-      console.log('The script work has been finished.');
-      if(err) { res.status(200).send({ error: err }); }
-      else { res.status(200).send({ message : 'Success' }); }
+    var dir_name = Date.now();
+    mkdirp('uploads/' + dir_name, function(err) {
+      var storage = multer.diskStorage({
+        destination: 'uploads/' + dir_name,
+        filename: function ( req, file, cb ) {
+            cb(null, file.originalname);
+        }
+      });
+      var upload = multer({ storage: storage }).any();
+      upload(req, res, function(err) {
+          if(err) {
+              return res.end("Error uploading file.");
+          }
+          else {
+            var files = req.files;
+            for (var i = 0; i < files.length; i++) {
+              console.log('type %s', files[i].mimetype);
+              console.log('original name：%s', files[i].originalname);
+              console.log('size：%s', files[i].size);
+              console.log('save path：%s', files[i].path);
+            }
+            var options = {
+              mode: 'json',
+              pythonOptions: ['-u'], // get print results in real-time
+              scriptPath: path.join(__dirname + "../../../python"),
+              args: ['uploads/' + dir_name]
+            };
+            var shell = new PythonShell('py.py', options);
+            shell.on('message', function (message) {
+              console.log(message);
+            });
+            shell.end(function (err) {
+              console.log('The script work has been finished.');
+              if(err) { res.status(200).send({ error: err }); }
+              else { res.status(200).send({ message : 'Success' }); }
+            });
+          }
+      });
     });
 }
