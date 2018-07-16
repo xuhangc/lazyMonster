7dbc084c06ce382dd58af726bbc3a9f166ac3d3f
Xuhang Chen <xuhangc@seas.upenn.edu>
2018-07-15 21:35:56 -0400

    update

--- config/routes.js
+++ config/routes.js
@@ -9,10 +9,10 @@ module.exports = function (app, passport) {
     app.get('/', home.loggedIn, home.home);//home
     app.get('/home', home.loggedIn, home.home);//home
 
-    app.get('/qc', home.loggedIn, home.qc);//qc
-    app.get('/raw', home.loggedIn, home.raw);//raw
-    app.get('/retest', home.loggedIn, home.retest);//retest
-    app.get('/eachqc', home.loggedIn, home.eachqc);//eachqc
+    // app.get('/qc', home.loggedIn, home.qc);//qc
+    // app.get('/raw', home.loggedIn, home.raw);//raw
+    // app.get('/retest', home.loggedIn, home.retest);//retest
+    // app.get('/eachqc', home.loggedIn, home.eachqc);//eachqc
 
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/home', // redirect to the secure profile section
